79b6677ee01edaaf6f2840c68725d68142d66ddc
Rahul <Rahul@LAPTOP-K4LPDN03>
2023-04-27 06:11:06 +0530

    secondcommit

---
+++ src/string/string1.java
@@ -0,0 +1,15 @@
+package string;
+
+public class string1 {
+public static void main(String[] args) {
+	String s1 = "GLOBAL";
+	String s2 = "GLOBAL";
+	if(s1.equals(s2)==true) {
+		System.out.println("strings are equal");
+	}
+	else {
+	System.out.println("strings are not equal");
+		
+	}
+}
+}
